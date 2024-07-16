d81b74a3ba6bf3c1e229bdb778e220df554df874
ibadifal <85802489+ibadfawa@users.noreply.github.com>
2024-07-16 20:07:01 +0800

    Update base.c

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -354,10 +354,15 @@ static int proc_pid_wchan(struct seq_file *m, struct pid_namespace *ns,
 	wchan = get_wchan(task);
 
 	if (wchan && ptrace_may_access(task, PTRACE_MODE_READ_FSCREDS)
-			&& !lookup_symbol_name(wchan, symname))
+			&& !lookup_symbol_name(wchan, symname)) {
+		  if (strstr(symname, "trace")) { 
+         seq_printf(m, "%s", "sys_epoll_wait"); 
+       } 
+          
 		seq_printf(m, "%s", symname);
-	else
+	}else{
 		seq_putc(m, '0');
+	}
 
 	return 0;
 }
