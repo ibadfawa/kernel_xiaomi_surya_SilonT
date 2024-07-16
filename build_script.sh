d2f51a5a5fc574bbe73b4e9e2f57ddd9e00c00b8
ibadifal <85802489+ibadfawa@users.noreply.github.com>
2024-07-16 20:32:26 +0800

    Update build_script.sh

--- build_script.sh
+++ build_script.sh
@@ -111,10 +111,10 @@ packingkernel() {
     fi
     git clone "$ANYKERNEL_REPO" -b "$ANYKERNEL_BRANCH" "${ANYKERNEL}"
     if ! [ -f "${KERN_IMG}" ]; then
-        build_failed
+       # build_failed
     fi
     if ! [ -f "${KERN_DTBO}" ]; then
-        build_failed
+       # build_failed
     fi
     if [[ "${DTB_TYPE}" =~ "single" ]]; then
         cp "${KERN_IMG}" "${ANYKERNEL}"/Image.gz-dtb
